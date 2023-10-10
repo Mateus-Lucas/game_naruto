4b46e8ae991eeda937c40eb8fa93e160decfb43d
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-10-10 18:17:15 -0300

    Começando a estilização dos cards

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { Card } from 'react-native-paper'; 
+import { Card } from 'react-native-paper';
 import Api from '../services/Api';
 
 export default function Home() {
@@ -9,14 +9,17 @@ export default function Home() {
       data={Api}
       keyExtractor={(item) => item.id.toString()}
       contentContainerStyle={styles.container}
-      numColumns={2} 
+      numColumns={2}
       renderItem={({ item }) => (
         <Card style={styles.card}>
           <Card.Cover source={{ uri: item.imagem }} style={styles.cardImage} />
           <Card.Content>
             <Text style={styles.cardTitle}>{item.nome}</Text>
             <Text style={styles.cardDescription}>
-              Clã: {item.clã}, Patente: {item.patente}
+              Clã: {item.clã}
+            </Text>
+            <Text style={styles.cardDescription}>
+              Patente: {item.patente}
             </Text>
           </Card.Content>
         </Card>
@@ -28,18 +31,21 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     padding: 16,
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#fff',
   },
   card: {
     margin: 8,
-    flex: 0.5, 
+    flex: 0.5,
     elevation: 4,
-    marginTop: 40
+    borderWidth: 6,
+    borderColor: '#DAA520',
+    backgroundColor: 'white',
+    borderRadius: 0
+    
   },
   cardImage: {
-    height: 150,
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
+    height: 120,
+    borderRadius: 0
   },
   cardTitle: {
     fontSize: 18,
