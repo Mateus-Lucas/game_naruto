86d2939a9b94798e18039db5e0559a7d1630ef0a
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-10-19 12:43:25 -0300

    Implementando seleção de personagem com problemas

--- src/screens/Jogo.js
+++ src/screens/Jogo.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, Button, Modal, Text, TouchableOpacity, Image } from 'react-native';
-import { Card, Title, Paragraph, ProgressBar } from 'react-native-paper'; // Importe o ProgressBar do react-native-paper
-import { useRoute } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, FlatList, Button, Modal, Text, TouchableOpacity } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
 import Api from '../services/Api';
 
 import aguaImage from '../img/agua.png';
@@ -11,146 +10,187 @@ import relampagoImage from '../img/relampago.png';
 import terraImage from '../img/terra.png';
 
 export default function Jogo() {
-  const route = useRoute();
-  const { ids } = route.params;
+  const [team1, setTeam1] = useState([]);
+  const [team2, setTeam2] = useState([]);
+  const [isTeam1Ready, setIsTeam1Ready] = useState(false);
+  const [isTeam2Ready, setIsTeam2Ready] = useState(false);
+  const [selectionPhase, setSelectionPhase] = useState('team1');
+  const [isModalVisible, setIsModalVisible] = useState(true);
 
-  const [selectedCharacters, setSelectedCharacters] = useState([]);
-  const [isChoosingCharacter, setIsChoosingCharacter] = useState(false);
-
-  const [characterLives, setCharacterLives] = useState({});
-  const vidaFixa = 25000;
+  const checkTeamIsComplete = (team) => {
+    return team.length === 3;
+  };
 
   useEffect(() => {
-    const filteredCharacters = Api.filter((character) => ids.includes(character.id));
-    setSelectedCharacters(filteredCharacters);
+    setIsTeam1Ready(checkTeamIsComplete(team1));
+    setIsTeam2Ready(checkTeamIsComplete(team2));
+    console.log('Time 1:', team1);
+    console.log('Time 2:', team2);
+  }, [team1, team2]);
 
-    const initialLives = {};
-    filteredCharacters.forEach((character) => {
-      initialLives[character.id] = vidaFixa;
-    });
-    setCharacterLives(initialLives);
-  }, [ids]);
-
-  const handleAttack = (atacante) => {
-    const defensor = selectedCharacters[0];
-    const dano = atacante.ataque;
-    const novaVida = characterLives[defensor.id] - dano;
-    setCharacterLives({
-      ...characterLives,
-      [defensor.id]: novaVida,
-    });
-  };
-
-  const getChacraImage = (naturezaDeChacra) => {
-    switch (naturezaDeChacra) {
-      case 'Água':
-        return aguaImage;
-      case 'Vento':
-        return ventoImage;
-      case 'Fogo':
-        return fogoImage;
-      case 'Relâmpago':
-        return relampagoImage;
-      case 'Terra':
-        return terraImage;
-      default:
-        return null;
-    }
-  };
+  const generateKey = (prefix, character) => `${prefix}_${character.id}`;
 
   const handleChooseCharacter = () => {
-    setIsChoosingCharacter(true);
+      setIsChoosingCharacter(true);
   };
 
   const handleCharacterSelection = (character) => {
-    setSelectedCharacters([...selectedCharacters, character]);
-    setIsChoosingCharacter(false);
+  if (selectionPhase === 'completed') {
+    setIsModalVisible(false);
+    return; // Não faz nada se a seleção estiver concluída.
+  }
+
+  if (selectionPhase === 'team1' && team1.length < 3) {
+    setTeam1([...team1, character]);
+  } else if (selectionPhase === 'team2' && team2.length < 3) {
+    setTeam2([...team2, character]);
+  }
+
+  if (team1.length === 3 && team2.length === 3) {
+    setIsModalVisible(false); // Fecha o modal
+    setSelectionPhase('completed');
+  } else if (selectionPhase === 'team1' && team1.length === 3) {
+    setSelectionPhase('team2');
+  }
+};
+
+
+
+  const getSelectionMessage = () => {
+    if (team1.length < 3) {
+      return 'Selecione personagens para o Time 1';
+    } else if (team2.length < 3) {
+      return 'Selecione personagens para o Time 2';
+    } else {
+      return 'Seleção concluída';
+    }
+  };
+
+
+  const availableCharacters = Api.filter((character) => {
+    return !team1.includes(character) && !team2.includes(character);
+  });
+
+
+  const handleResetSelection = () => {
+    setTeam1([]);
+    setTeam2([]);
+    setSelectionPhase('team1');
+    setIsChoosingCharacter(true);
+  };
+
+
+
+  const closeModal = () => {
+    setIsModalVisible(false);
   };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={selectedCharacters}
-        keyExtractor={(item) => item.id.toString()}
-        contentContainerStyle={styles.characterList}
-        renderItem={({ item }) => (
-          <View style={styles.characterCardContainer}>
+      {isTeam1Ready && (
+        <FlatList
+          data={team1}
+          horizontal={true}
+          keyExtractor={(item) => generateKey('team1', item)}
+          renderItem={({ item }) => (
             <Card style={styles.card}>
-              <Card style={styles.cardImagem}>
+              <Card style={{ backgroundColor: '#600000' }}>
                 <Title style={styles.characterName}>{item.nome}</Title>
-                <Card.Cover
-                  source={{ uri: item.imagem }}
-                  style={styles.image}
-                />
-                <View style={{ position: 'relative' }}>
-                  <Image
-                    source={getChacraImage(item.natureza_de_chacra)}
-                    style={styles.imageNat}
-                  />
-                </View>
+                <Card.Cover source={{ uri: item.imagem }} style={styles.image} />
               </Card>
               <Paragraph style={{ fontWeight: 'bold', color: 'white' }}>
-                <Text>Patente: </Text>
-                {item.patente}
+                <Text>Patente: {item.patente}</Text>
               </Paragraph>
               <Paragraph style={{ fontWeight: 'bold', color: 'white' }}>
-                <Text >Jutsu Especial: </Text>
-                {item.jutsu_especial}
+                <Text>Jutsu Especial: {item.jutsu_especial}</Text>
               </Paragraph>
               <Card style={styles.cardAtributos}>
                 <Paragraph style={styles.textCard}>
-                  <Text style={styles.textStyle}>Ataque: ................. </Text>
-                  <Text style={styles.atributosStyle}>{item.ataque}</Text>
+                  <Text style={styles.textStyle}>Ataque: {item.ataque}</Text>
                 </Paragraph>
                 <Paragraph style={styles.textCard}>
-                  <Text style={styles.textStyle}>Defesa: ................. </Text>
-                  <Text style={styles.atributosStyle}>{item.defesa}</Text>
+                  <Text style={styles.textStyle}>Defesa: {item.defesa}</Text>
                 </Paragraph>
                 <Paragraph style={styles.textCard}>
-                  <Text style={styles.textStyle}>Especial: ............... </Text>
-                  <Text style={styles.atributosStyle}>{item.dano_especial}</Text>
+                  <Text style={styles.textStyle}>Especial: {item.dano_especial}</Text>
                 </Paragraph>
               </Card>
-              <Button
-                title="Atacar"
-                onPress={() => handleAttack(item)}
-              />
             </Card>
-            <Text style={styles.atributosStyle}>
-              {characterLives[item.id]}
-            </Text>
-            <ProgressBar
-              progress={characterLives[item.id] / vidaFixa} 
-              color={'blue'} 
-              style={{height: 15}}
-            />
-          </View>
-        )}
-      />
+          )}
+        />
+      )}
+
+      {isTeam2Ready && (
+        <FlatList
+          data={team2}
+          horizontal={true}
+          keyExtractor={(item) => generateKey('team2', item)}
+          renderItem={({ item }) => (
+            <Card style={styles.card}>
+              <Card style={{ backgroundColor: '#600000' }}>
+                <Title style={styles.characterName}>{item.nome}</Title>
+                <Card.Cover source={{ uri: item.imagem }} style={styles.image} />
+              </Card>
+              <Paragraph style={{ fontWeight: 'bold', color: 'white' }}>
+                <Text>Patente: {item.patente}</Text>
+              </Paragraph>
+              <Paragraph style={{ fontWeight: 'bold', color: 'white' }}>
+                <Text>Jutsu Especial: {item.jutsu_especial}</Text>
+              </Paragraph>
+              <Card style={styles.cardAtributos}>
+                <Paragraph style={styles.textCard}>
+                  <Text style={styles.textStyle}>Ataque: {item.ataque}</Text>
+                </Paragraph>
+                <Paragraph style={styles.textCard}>
+                  <Text style={styles.textStyle}>Defesa: {item.defesa}</Text>
+                </Paragraph>
+                <Paragraph style={styles.textCard}>
+                  <Text style={styles.textStyle}>Especial: {item.dano_especial}</Text>
+                </Paragraph>
+              </Card>
+            </Card>
+          )}
+        />
+      )}
+
       <Button
         title="Escolher Personagem"
         onPress={handleChooseCharacter}
       />
       <Modal
-        visible={isChoosingCharacter}
+        visible={selectionPhase !== 'completed'}
         animationType="slide"
         transparent={true}
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <Text>Escolha um novo personagem:</Text>
+            <Title style={styles.titleModal}>{getSelectionMessage()}
+
+              <Button title="Fechar Modal" onPress={closeModal} />
+            </Title>
+
             <FlatList
-              data={Api}
+              data={availableCharacters}
               keyExtractor={(item) => item.id.toString()}
               renderItem={({ item }) => (
                 <TouchableOpacity onPress={() => handleCharacterSelection(item)}>
-                  <Text>{item.nome}</Text>
+                  <Card style={styles.cardModal}>
+                    <Title style={{ color: 'white' }}>{item.nome}</Title>
+                    <Card.Cover source={{ uri: item.imagem }} style={styles.imageModal} />
+                  </Card>
                 </TouchableOpacity>
               )}
             />
+
           </View>
         </View>
       </Modal>
+
+      <Button
+        title="Reset"
+        onPress={handleResetSelection}
+      />
+
     </View>
   );
 }
@@ -160,17 +200,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'black'
   },
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+
   },
   modalContent: {
     width: '80%',
-    backgroundColor: 'white',
+    backgroundColor: 'black',
     padding: 16,
     borderRadius: 8,
+    borderWidth: 5,
+    borderColor: 'red',
+    marginBottom: 50
+  },
+  titleModal: {
+    marginBottom: 10,
+    marginTop: 50,
+    color: 'white'
+  },
+  cardModal: {
+    borderRadius: 10,
+    backgroundColor: 'red',
+    padding: 10,
+    marginBottom: 10
   },
   card: {
     borderWidth: 8,
@@ -184,7 +240,7 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 0,
     backgroundColor: '#600000',
     marginBottom: 15,
-    borderWidth: 2
+    borderWidth: 2,
   },
   imageNat: {
     width: 35,
@@ -196,7 +252,7 @@ const styles = StyleSheet.create({
   },
   textStyle: {
     fontWeight: 'bold',
-    color: 'white'
+    color: 'white',
   },
   cardAtributos: {
     backgroundColor: 'black',
@@ -215,10 +271,10 @@ const styles = StyleSheet.create({
   },
   image: {
     borderRadius: 0,
-    height: 130
+    height: 130,
   },
   characterName: {
     margin: 5,
-    color: 'white'
+    color: 'white',
   },
 });
