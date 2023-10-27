526198d6ec04b6494e2d382853521f6c820bb076
pedro-iesb <pedro.santos3@iesb.edu.br>
2023-10-27 15:01:30 -0300

    npm run

--- src/services/Api.js
+++ src/services/Api.js
@@ -20,6 +20,7 @@ const Api = [
         "patente": "Ninja Renegado",
         "defesa": 850,
         "ataque": 970,
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fwww.looper.com%2Fimg%2Fgallery%2Fevery-power-sasuke-has-on-naruto-explained%2Fintro-1663193400.jpg&tbnid=kOvBIwBOQZsRHM&vet=12ahUKEwiFvfXg5ZaCAxXoR7gEHbS9Do0QMyhDegUIARCSAg..i&imgrefurl=https%3A%2F%2Fwww.looper.com%2F1009943%2Fevery-power-sasuke-has-on-naruto-explained%2F&docid=rpGwmAtjBqsSwM&w=780&h=438&q=sasuke%20uchiha&hl=pt-BR&ved=2ahUKEwiFvfXg5ZaCAxXoR7gEHbS9Do0QMyhDegUIARCSAg",
         "id": 2
     },
     {
@@ -67,6 +68,7 @@ const Api = [
         "patente": "Sannin",
         "defesa": 920,
         "ataque": 950,
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fkanto.legiaodosherois.com.br%2Fw750-h450-k1%2Fwp-content%2Fuploads%2F2021%2F02%2Flegiao_jnhcQRAHmVtP.jpg.webp&tbnid=J4zG7tc8WeJuvM&vet=12ahUKEwjbs8XH5ZaCAxVBtZUCHZS3CJ8QMygIegUIARCCAQ..i&imgrefurl=https%3A%2F%2Fwww.legiaodosherois.com.br%2Fquiz%2Fnaruto-jiraiya.html&docid=gTQKjng1jMB-TM&w=750&h=392&q=jiraya%20naruro&hl=pt-BR&ved=2ahUKEwjbs8XH5ZaCAxVBtZUCHZS3CJ8QMygIegUIARCCAQ",
         "id": 6
     },
     {
@@ -78,7 +80,7 @@ const Api = [
         "patente": "Jonin",
         "defesa": 800,
         "ataque": 870,
-        "imagem": "https://example.com/hinata.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fi0.wp.com%2Fborutoexplorer.com.br%2Fwp-content%2Fuploads%2F2020%2F08%2FWhatsApp-Image-2020-08-16-at-11.58.39.jpeg%3Fresize%3D800%252C450%26ssl%3D1&tbnid=1CExmRXaTZS89M&vet=12ahUKEwi4vdHe35aCAxWJQ7gEHcvRD9MQMygEegQIARB0..i&imgrefurl=https%3A%2F%2Fborutoexplorer.com.br%2Freview-hinata-hyuuga%2F&docid=GBLW4rSsHABnXM&w=800&h=450&q=hinata%20hyuga%20&hl=pt-BR&ved=2ahUKEwi4vdHe35aCAxWJQ7gEHcvRD9MQMygEegQIARB0",
         "id": 7
     },
     {
@@ -90,7 +92,7 @@ const Api = [
         "patente": "Jonin",
         "defesa": 800,
         "ataque": 820,
-        "imagem": "https://example.com/shikamaru.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fcriticalhits.com.br%2Fwp-content%2Fuploads%2F2021%2F12%2FShikamaru-1.jpg&tbnid=v7_kLf6NTADoHM&vet=12ahUKEwi6uKL135aCAxXaq5UCHeg4CBQQMygHegQIARB9..i&imgrefurl=https%3A%2F%2Fcriticalhits.com.br%2Fanime%2Fquais-transformacoes-da-natureza-shikamaru-consegue-utilizar-em-naruto%2F&docid=9yH0EgzpTzVB7M&w=1259&h=716&q=shikamaru%20nara&hl=pt-BR&ved=2ahUKEwi6uKL135aCAxXaq5UCHeg4CBQQMygHegQIARB9",
         "id": 8
     },
     {
@@ -102,7 +104,7 @@ const Api = [
         "patente": "Chunin",
         "defesa": 950,
         "ataque": 800,
-        "imagem": "https://example.com/rock-lee.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Frevolucaonerd.com%2Fwordpress%2Fwp-content%2Ffiles%2Frevolucaonerd.com%2F2023%2F02%2Frock-lee-vs-gaara.webp&tbnid=t9whyLO2NhASsM&vet=12ahUKEwi-18WO4JaCAxXTp5UCHTdoB9cQMygEegQIARB3..i&imgrefurl=https%3A%2F%2Frevolucaonerd.com%2Fqual-ep-rock-lee-luta-com-gaara%2F&docid=3tmULd6tgywWdM&w=1200&h=800&q=rock%20lee&hl=pt-BR&ved=2ahUKEwi-18WO4JaCAxXTp5UCHTdoB9cQMygEegQIARB3",
         "id": 9
     },
     {
@@ -114,7 +116,7 @@ const Api = [
         "patente": "jounin",
         "defesa": 960,
         "ataque": 850,
-        "imagem": "https://example.com/Might Guy.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fnaruto%2Fimages%2Fb%2Fb1%2FGuy_abre_o_Oitavo_Port%25C3%25A3o.PNG%2Frevision%2Flatest%3Fcb%3D20150709184147%26path-prefix%3Dpt-br&tbnid=8lQVf71KmAnDaM&vet=12ahUKEwicsPua4JaCAxUgqpUCHex6AyAQMygCegQIARBO..i&imgrefurl=https%3A%2F%2Fnaruto.fandom.com%2Fpt-br%2Fwiki%2FMight_Guy&docid=BI4RbYdYeLzrBM&w=1920&h=1080&q=Might%20Guy&hl=pt-BR&ved=2ahUKEwicsPua4JaCAxUgqpUCHex6AyAQMygCegQIARBO",
         "id": 10
     },
     {
@@ -126,7 +128,7 @@ const Api = [
         "patente": "Jonin",
         "defesa": 870,
         "ataque": 820,
-        "imagem": "https://example.com/tenten.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fcriticalhits.com.br%2Fwp-content%2Fuploads%2F2021%2F11%2F10-perguntas-fatos-e-curiosidades-sobre-Tenten-1.jpg&tbnid=V8bJYeakf1gGWM&vet=12ahUKEwiR9a654JaCAxUou5UCHcn2AbUQMygFegQIARBv..i&imgrefurl=https%3A%2F%2Fcriticalhits.com.br%2Fanime%2Ftenten-conseguiria-derrotar-a-sakura-em-naruto%2F&docid=L3dx_Hp9A0MmtM&w=1280&h=720&q=ten%20ten&hl=pt-BR&ved=2ahUKEwiR9a654JaCAxUou5UCHcn2AbUQMygFegQIARBv",
         "id": 11
     },
     {
@@ -138,7 +140,7 @@ const Api = [
         "patente": "Jonin",
         "defesa": 900,
         "ataque": 950,
-        "imagem": "https://example.com/neji.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fwww.einerd.com.br%2Fwp-content%2Fuploads%2F2019%2F10%2FBoruto-encontro-Neji-capa-890x465.png&tbnid=-_6FEI7a_fIMxM&vet=12ahUKEwjS59jE4JaCAxV-hJUCHb-NC_EQMygEegQIARB3..i&imgrefurl=https%3A%2F%2Fwww.einerd.com.br%2Fboruto-neji-hyuga-manga%2F&docid=uwZW_gpEQVin5M&w=890&h=465&q=neji%20hyuga&hl=pt-BR&ved=2ahUKEwjS59jE4JaCAxV-hJUCHb-NC_EQMygEegQIARB3",
         "id": 12
     },
     {
@@ -150,7 +152,7 @@ const Api = [
         "patente": "Chunin",
         "defesa": 870,
         "ataque": 980,
-        "imagem": "https://example.com/kiba.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fnaruto%2Fimages%2Fb%2Fb1%2FKiba_%2528Parte_II%2529.png%2Frevision%2Flatest%3Fcb%3D20170530230546%26path-prefix%3Dpt-br&tbnid=3ke_ocvxZwDMFM&vet=12ahUKEwie3OLV4JaCAxVpu5UCHUvHBwAQMygBegQIARBF..i&imgrefurl=https%3A%2F%2Fnaruto.fandom.com%2Fpt-br%2Fwiki%2FKiba_Inuzuka&docid=ud4_SoZ9FHsVkM&w=1920&h=1080&q=kiba%20inuzuka%20shippuden&hl=pt-BR&ved=2ahUKEwie3OLV4JaCAxVpu5UCHUvHBwAQMygBegQIARBF",
         "id": 13
     },
     {
@@ -162,8 +164,32 @@ const Api = [
         "patente": "Jonin",
         "defesa": 920,
         "ataque": 890,
-        "imagem": "https://example.com/shino.png",
+        "imagem": "https://www.google.com.br/imgres?imgurl=http%3A%2F%2F2.bp.blogspot.com%2F-vgBRNC97qtM%2FTifDK4di5vI%2FAAAAAAAAAgg%2Fowo345z_b-0%2Fs1600%2FShino-Aburame-shino-aburame-17766089-1280-720.jpg&tbnid=M3rVIraYgaHQYM&vet=12ahUKEwju36Dm4JaCAxWHTbgEHYggBcUQMygIegQIARB-..i&imgrefurl=http%3A%2F%2Fclubenarutoblogspostcom.blogspot.com%2F2011%2F07%2Faburame-shino.html&docid=neD3mo8y3o86xM&w=1280&h=720&q=shino%20aburame&hl=pt-BR&ved=2ahUKEwju36Dm4JaCAxWHTbgEHYggBcUQMygIegQIARB-",
         "id": 14
+    },
+    {
+        "nome": "itachi uchiha",
+        "clã": "Uchiha",
+        "jutsu_especial": "Amaterasu",
+        "dano_especial": 960,
+        "natureza_de_chacra": "fogo",
+        "patente": "Jonin",
+        "defesa": 920,
+        "ataque": 890,
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fnaruto%2Fimages%2F2%2F24%2FItachi-Uchiha.png%2Frevision%2Flatest%3Fcb%3D20181104200812%26path-prefix%3Dpt-br&tbnid=NyMu1FFVscL4YM&vet=12ahUKEwji0OST4ZaCAxVrjZUCHedmC3sQMygAegQIARBv..i&imgrefurl=https%3A%2F%2Fnaruto.fandom.com%2Fpt-br%2Fwiki%2FItachi_Uchiha&docid=4OnH-8wpQwKpOM&w=1402&h=1061&q=itachi%20uchiha&hl=pt-BR&ved=2ahUKEwji0OST4ZaCAxVrjZUCHedmC3sQMygAegQIARBv",
+        "id": 15
+    },
+    {
+        "nome": "Kisame Hoshigaki",
+        "clã": "Hoshigaki",
+        "jutsu_especial": "Suiton Senshokukō",
+        "dano_especial": 960,
+        "natureza_de_chacra": "água",
+        "patente": "Jonin",
+        "defesa": 920,
+        "ataque": 890,
+        "imagem": "https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fimages3.alphacoders.com%2F665%2F665366.jpg&tbnid=AlTzYXdkW17cDM&vet=12ahUKEwiU-9Tk4paCAxUtipUCHUARDQIQMygEegQIARB3..i&imgrefurl=https%3A%2F%2Fwall.alphacoders.com%2Ftag%2Fkisame-hoshigaki-wallpapers%3Flang%3DPortuguese&docid=yu6Fw91IwyZQQM&w=2560&h=1600&q=kisame%20&hl=pt-BR&ved=2ahUKEwiU-9Tk4paCAxUtipUCHUARDQIQMygEegQIARB3",
+        "id": 16
     },
 
 ]
